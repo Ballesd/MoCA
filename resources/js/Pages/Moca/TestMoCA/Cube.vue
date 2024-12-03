4af5780eaa816f0a96acf2cb74ec92f7a100a8fa
aledezma25 <aledezma@unal.edu.co>
2024-12-03 15:26:44 -0500

    Se modifica algunos componentes para darle responsividad

--- resources/js/Pages/Moca/TestMoCA/Cube.vue
+++ resources/js/Pages/Moca/TestMoCA/Cube.vue
@@ -1,17 +1,33 @@
 <template>
-    <div class="flex items-center justify-center space-x-12 my-12">
-        <div class="w-8/12 flex flex-col gap-4">
+    <div class="flex flex-col lg:flex-row items-center justify-center lg:space-x-12 space-y-6 lg:space-y-0 my-12">
+        <!-- Contenedor de Texto -->
+        <div class="w-full lg:w-8/12 flex flex-col gap-4 px-4">
             <div class="flex justify-center items-center space-x-3">
-                <font-awesome-icon :icon="['fas', 'volume-up']" size="2x" class="text-secondary cursor-pointer hover:text-primary" @click="speachIntroduction" />
-                <h2 class="text-primary text-3xl">2. Capacidades visoconstructivas</h2>
+                <font-awesome-icon
+                    :icon="['fas', 'volume-up']"
+                    size="2x"
+                    class="text-secondary cursor-pointer hover:text-primary"
+                    @click="speachIntroduction"
+                />
+                <h2 class="text-primary text-xl sm:text-2xl md:text-3xl text-center lg:text-left">
+                    2. Capacidades visoconstructivas
+                </h2>
             </div>
             <div class="border-2 border-gray-400 rounded-lg p-4">
-                <p class="text-gray-500 text-center">Copie esta figura lo mejor posible.</p>
+                <p class="text-gray-500 text-sm sm:text-base text-center">
+                    Copie esta figura lo mejor posible.
+                </p>
             </div>
             <UploadFile :uploadedImage="uploadedImage" />
         </div>
-        <div class="p-4 h-64">
-            <img class="w-full h-full" src="../../../../../public/images/cube.png" alt="Logo" />
+
+        <!-- Contenedor de Imagen -->
+        <div class="p-4 h-56 w-full lg:w-auto">
+            <img
+                class="w-full h-full object-contain lg:object-cover rounded-lg max-w-xs sm:max-w-sm md:max-w-md lg:max-w-none"
+                src="../../../../../public/images/cube.png"
+                alt="Logo"
+            />
         </div>
     </div>
 </template>
@@ -26,11 +42,12 @@ const { sendNumber } = defineProps(['sendNumber']);
 const result = ref(null);
 
 const speachIntroduction = () => {
-    const text1 = 'Me gustaría que copiara el dibujo de un cubo tal cual como está en la pantalla de la manera más precisa posible, posteriormente suba la imagen.';
+    const text1 =
+        'Me gustaría que copiara el dibujo de un cubo tal cual como está en la pantalla de la manera más precisa posible, posteriormente suba la imagen.';
     const synthesis = window.speechSynthesis;
     const utterance = new SpeechSynthesisUtterance(text1);
     utterance.rate = 0.7;
-    utterance.lang = "es-CO" 
+    utterance.lang = 'es-CO';
     synthesis.speak(utterance);
 };
 
@@ -39,8 +56,8 @@ const uploadedImage = async (fileInfo) => {
     formData.append('image_cube', fileInfo);
     const response = await axios.post('/moca/uploadcube', formData, {
         headers: {
-            'Content-Type': 'multipart/form-data'
-        }
+            'Content-Type': 'multipart/form-data',
+        },
     });
     result.value = null;
     sendNumber(result.value);
