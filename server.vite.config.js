589f4c51e7143329349ddac8776250cd903a572f
aledezma25 <aledezma@unal.edu.co>
2024-12-25 22:39:44 -0500

    Se actualiza el readme, y se crea archivo para probar en movil

--- server.vite.config.js
+++ server.vite.config.js
@@ -3,6 +3,13 @@ import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0', // Permite escuchar todas las conexiones
+        port: 5173,      // Cambia el puerto si es necesario
+        hmr: {
+            host: '192.168.101.16', // Cambia la IP si es necesario
+        },
+    },
     plugins: [
         laravel({
             input: 'resources/js/app.js',
